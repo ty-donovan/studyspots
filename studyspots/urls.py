cb9cb5cdb3d873df524f906ce00a763d7b3f3a05
paulad <yqy6yn@virginia.edu>
2023-10-08 11:58:33 -0400

    fixing urls

--- studyspots/urls.py
+++ studyspots/urls.py
@@ -9,6 +9,4 @@ urlpatterns = [
     path("map/", views.map, name="map"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("login/", views.profile, name="welcome"),
-    path('welcome-admin/', views.welcome_admin, name='welcome-admin'),
-    path('welcome-user/', views.welcome_user, name='welcome-user'),
 ]
