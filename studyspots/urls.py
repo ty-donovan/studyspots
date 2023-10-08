a265bc940548ae847e9da1a7d5ed809ff7e1a32c
paulad <yqy6yn@virginia.edu>
2023-10-08 18:13:10 -0400

    fixing index

--- studyspots/urls.py
+++ studyspots/urls.py
@@ -5,10 +5,8 @@ from . import views
 
 app_name = "studyspots"
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("home/", views.index, name="index"),
     path("map/", views.map, name="map"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("login/", views.profile, name="welcome"),
-    path('welcome-admin/', views.welcome_admin, name='welcome-admin'),
-    path('welcome-user/', views.welcome_user, name='welcome-user'),
 ]
--- studyspots/views.py
+++ studyspots/views.py
@@ -11,22 +11,6 @@ def profile(request):
         return render(request, 'studyspots/welcome/user.html', {'username': request.user.username})
 
 
-@login_required
-def welcome_admin(request):
-    if request.user.user_role == 'admin':
-        return render(request, 'studyspots/welcome/admin.html', {'username': request.user.username})
-    else:
-        return render(request, 'studyspots/welcome/user.html', {'username': request.user.username})
-
-
-@login_required
-def welcome_user(request):
-    if request.user.user_role == 'user':
-        return render(request, 'studyspots/welcome/user.html', {'username': request.user.username})
-    else:
-        return render(request, 'studyspots/welcome/admin.html', {'username': request.user.username})
-
-
 def index(request):
     return render(request, 'studyspots/index.html')
 
