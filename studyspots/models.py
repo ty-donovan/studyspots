469d07efd5145d09d56c8f6e95bbe7cbbbe97c93
paulad <yqy6yn@virginia.edu>
2023-10-08 04:49:54 -0400

    loading different admin and user pages

--- studyspots/models.py
+++ studyspots/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    user_role = models.CharField(max_length=10, choices=[('admin', 'Admin'), ('user', 'User')], default='user')
+
+    def __str__(self):
+        return self.username
+
+
--- studyspots/views.py
+++ studyspots/views.py
@@ -4,16 +4,23 @@ from django.contrib.auth.decorators import login_required
 
 
 @login_required
-def profile(request):
-    if request.user.is_staff:
+def welcome_admin(request):
+    if request.user.user_role == 'admin':
         return render(request, 'studyspots/welcome/admin.html', {'username': request.user.username})
     else:
         return render(request, 'studyspots/welcome/user.html', {'username': request.user.username})
 
 
+@login_required
+def welcome_user(request):
+    if request.user.user_role == 'user':
+        return render(request, 'studyspots/welcome/user.html', {'username': request.user.username})
+    else:
+        return render(request, 'studyspots/welcome/admin.html', {'username': request.user.username})
+
+
 def index(request):
     return render(request, 'studyspots/index.html')
-
 
 
 def map(request):
