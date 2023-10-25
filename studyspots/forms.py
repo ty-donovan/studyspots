fc0568f41fdcfef57956c2a8b145ee6d3d4f1ad1
Charlotte Zhang <ksw6ux@virginia.edu>
2023-10-24 20:10:57 -0400

    changed ratings to enum

--- studyspots/forms.py
+++ studyspots/forms.py
@@ -10,14 +10,22 @@ TYPE_CHOICES = [
     ('Other', 'Other'),
 ]
 
+RATING_CHOICES = [
+    (1, '1'),
+    (2, '2'),
+    (3, '3'),
+    (4, '4'),
+    (5, '5'),
+]
+
 class LocationForm(forms.Form):
     name = forms.CharField(label='Name of Location', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
     location_type = forms.ChoiceField(choices=TYPE_CHOICES, widget=forms.Select(attrs={'class': 'form-select'}))
     on_grounds = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'class': 'form-check-input'}))
     description = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}), required=False)
-    overall_rating = forms.IntegerField(min_value=1, max_value=5, widget=forms.NumberInput(attrs={'class': 'form-control'}))
-    comfort_rating = forms.IntegerField(min_value=1, max_value=5, widget=forms.NumberInput(attrs={'class': 'form-control'}))
-    noise_level_rating = forms.IntegerField(min_value=1, max_value=5, widget=forms.NumberInput(attrs={'class': 'form-control'}))
-    crowdedness_rating = forms.IntegerField(min_value=1, max_value=5, widget=forms.NumberInput(attrs={'class': 'form-control'}))
+    overall_rating = forms.ChoiceField(choices=RATING_CHOICES, widget=forms.Select(attrs={'class': 'form-select'}))
+    comfort_rating = forms.ChoiceField(choices=RATING_CHOICES, widget=forms.Select(attrs={'class': 'form-select'}))
+    noise_level_rating = forms.ChoiceField(choices=RATING_CHOICES, widget=forms.Select(attrs={'class': 'form-select'}))
+    crowdedness_rating = forms.ChoiceField(choices=RATING_CHOICES, widget=forms.Select(attrs={'class': 'form-select'}))
     lat = forms.DecimalField(widget=forms.HiddenInput(attrs={'class': 'form-control'}))
     lng = forms.DecimalField(widget=forms.HiddenInput(attrs={'class': 'form-control'}))
