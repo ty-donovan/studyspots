ac730a4fffe12851baf36812733d618d34a18433
Henry Evans <hhe2gnx@virginia.edu>
2023-10-30 21:33:25 -0400

    made fields required

--- studyspots/forms.py
+++ studyspots/forms.py
@@ -27,30 +27,109 @@ class SelectExistingLocationForm(forms.Form):
         required=False
     )
 
+
+
 class newLocationForm(forms.Form):
     # These are the fields to add a location
-    locationName = forms.CharField(label='Name of Location', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    location_type = forms.ChoiceField(choices=TYPE_CHOICES, widget=forms.Select(attrs={'class': 'form-select'}))
-    on_grounds = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'class': 'form-check-input'}))
-    lat = forms.DecimalField(widget=forms.HiddenInput(attrs={'class': 'form-control'}))
-    lng = forms.DecimalField(widget=forms.HiddenInput(attrs={'class': 'form-control'}))
+    locationName = forms.CharField(
+        label='Name of Location',
+        max_length=100,
+        widget=forms.TextInput(attrs={'class': 'form-control'}),
+        required=True  # Mark the field as required
+    )
+    location_type = forms.ChoiceField(
+        choices=TYPE_CHOICES,
+        widget=forms.Select(attrs={'class': 'form-select'}),
+        required=True  # Mark the field as required
+    )
+    on_grounds = forms.BooleanField(
+        required=False,
+        widget=forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+    )
+    lat = forms.DecimalField(
+        widget=forms.HiddenInput(attrs={'class': 'form-control'}),
+        required=True  # Mark the field as required
+    )
+    lng = forms.DecimalField(
+        widget=forms.HiddenInput(attrs={'class': 'form-control'}),
+        required=True  # Mark the field as required
+    )
     # These are the fields to add a spot
 
-    spotName = forms.CharField(label='Name of Spot', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    capacity = forms.IntegerField(label='Capacity', widget=forms.NumberInput(attrs={'class': 'form-control'}), max_value=1000)
-    comment = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}), required=False)
-    overall_rating = forms.ChoiceField(choices=RATING_CHOICES, widget=forms.Select(attrs={'class': 'form-select'}))
-    comfort_rating = forms.ChoiceField(choices=RATING_CHOICES, widget=forms.Select(attrs={'class': 'form-select'}))
-    noise_level_rating = forms.ChoiceField(choices=RATING_CHOICES, widget=forms.Select(attrs={'class': 'form-select'}))
-    crowdedness_rating = forms.ChoiceField(choices=RATING_CHOICES, widget=forms.Select(attrs={'class': 'form-select'}))
-    
+    spotName = forms.CharField(
+        label='Name of Spot',
+        max_length=100,
+        widget=forms.TextInput(attrs={'class': 'form-control'}),
+        required=True  # Mark the field as required
+    )
+    capacity = forms.IntegerField(
+        label='Capacity',
+        widget=forms.NumberInput(attrs={'class': 'form-control'}),
+        max_value=1000,
+        required=True  # Mark the field as required
+    )
+    comment = forms.CharField(
+        widget=forms.Textarea(attrs={'class': 'form-control'}),
+        required=False
+    )
+    overall_rating = forms.ChoiceField(
+        choices=RATING_CHOICES,
+        widget=forms.Select(attrs={'class': 'form-select'}),
+        required=True  # Mark the field as required
+    )
+    comfort_rating = forms.ChoiceField(
+        choices=RATING_CHOICES,
+        widget=forms.Select(attrs={'class': 'form-select'}),
+        required=True  # Mark the field as required
+    )
+    noise_level_rating = forms.ChoiceField(
+        choices=RATING_CHOICES,
+        widget=forms.Select(attrs={'class': 'form-select'}),
+        required=True  # Mark the field as required
+    )
+    crowdedness_rating = forms.ChoiceField(
+        choices=RATING_CHOICES,
+        widget=forms.Select(attrs={'class': 'form-select'}),
+        required=True  # Mark the field as required
+    )
+
 
 
 class existingLocationForm(forms.Form):
-    spotName = forms.CharField(label='Name of Spot', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    capacity = forms.IntegerField(label='Capacity', widget=forms.NumberInput(attrs={'class': 'form-control'}), max_value=1000)
-    comment = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}), required=False)
-    overall_rating = forms.ChoiceField(choices=RATING_CHOICES, widget=forms.Select(attrs={'class': 'form-select'}))
-    comfort_rating = forms.ChoiceField(choices=RATING_CHOICES, widget=forms.Select(attrs={'class': 'form-select'}))
-    noise_level_rating = forms.ChoiceField(choices=RATING_CHOICES, widget=forms.Select(attrs={'class': 'form-select'}))
-    crowdedness_rating = forms.ChoiceField(choices=RATING_CHOICES, widget=forms.Select(attrs={'class': 'form-select'}))
+    
+    spotName = forms.CharField(
+        label='Name of Spot',
+        max_length=100,
+        widget=forms.TextInput(attrs={'class': 'form-control'}),
+        required=True  # Mark the field as required
+    )
+    capacity = forms.IntegerField(
+        label='Capacity',
+        widget=forms.NumberInput(attrs={'class': 'form-control'}),
+        max_value=1000,
+        required=True  # Mark the field as required
+    )
+    comment = forms.CharField(
+        widget=forms.Textarea(attrs={'class': 'form-control'}),
+        required=False
+    )
+    overall_rating = forms.ChoiceField(
+        choices=RATING_CHOICES,
+        widget=forms.Select(attrs={'class': 'form-select'}),
+        required=True  # Mark the field as required
+    )
+    comfort_rating = forms.ChoiceField(
+        choices=RATING_CHOICES,
+        widget=forms.Select(attrs={'class': 'form-select'}),
+        required=True  # Mark the field as required
+    )
+    noise_level_rating = forms.ChoiceField(
+        choices=RATING_CHOICES,
+        widget=forms.Select(attrs={'class': 'form-select'}),
+        required=True  # Mark the field as required
+    )
+    crowdedness_rating = forms.ChoiceField(
+        choices=RATING_CHOICES,
+        widget=forms.Select(attrs={'class': 'form-select'}),
+        required=True  # Mark the field as required
+    )
