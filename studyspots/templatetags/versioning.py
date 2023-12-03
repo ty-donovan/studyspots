0436ec348c3fabbaf5ff2943c3194177362ab097
pharingWell <pharingwell@gmail.com>
2023-12-03 01:50:52 -0500

    Updated versioning to store the hash

--- studyspots/templatetags/versioning.py
+++ studyspots/templatetags/versioning.py
@@ -1,13 +1,29 @@
+import os
+
 import git
 from django import template
 from django.utils.html import format_html
+from git import InvalidGitRepositoryError
 
-repo = git.Repo(search_parent_directories=True)
-sha = repo.head.object.hexsha[:7]
+
+try:
+    repo = git.Repo(search_parent_directories=True)
+    sha = repo.head.object.hexsha[:7]
+    with open('studyspots/static/studyspots/git-hash.txt', 'w') as f:
+        f.write(sha)
+except InvalidGitRepositoryError:
+    try:
+        with open('studyspots/staticfiles/studyspots/git-hash.txt') as f:
+            sha = f.read()
+    except FileNotFoundError:
+        sha = None
 
 register = template.Library()
 
 
 @register.simple_tag()
 def git_hash():
-    return format_html('<p class="mb-0 text-secondary footer-text">Hash {}</p>', str(sha))
+    if sha:
+        return format_html('<p class="mb-0 text-secondary footer-text">Hash {}</p>', str(sha))
+    else:
+        return ""
